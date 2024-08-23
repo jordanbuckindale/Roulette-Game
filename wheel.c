489da2580cd0983b211ae01c2c56af2519bc55f0
Jordan Buckindale <94644161+jordanbuckindale@users.noreply.github.com>
2024-08-22 21:06:32 -0700

    Update wheel.c and rm junk code

--- wheel.c
+++ wheel.c
@@ -11,21 +11,6 @@ used in demo
 
 #define WHEEL_SIZE 400
 
-/* 
-Wheel structure.
-
-Data: 
--> angle (angle to rotate wheel)
--> spinning (boolean: true/false)
--> *drawing_area (GTK widget pointer)
-*/
-// typedef struct {
-//     double angle; //-> angle (angle to rotate wheel)
-//     int spinning; //-> spinning (boolean: true/false)
-//     double initial_speed; //-> speed of spin
-//     double spin_duration;//-> duration of spin
-//     GtkWidget *drawing_area; //-> *drawing_area (GTK widget pointer)
-// } WheelState;
 
 /*
 Function to create the draw object. 
@@ -79,9 +64,9 @@ gboolean on_draw(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
         cairo_fill(cr);
 
         // line in roulette table
-        // cairo_set_source_rgb(cr, 1, 1, 1); //set color back to white
+        // cairo_set_source_rgb(cr, 1, 0, 1); //set color back to white
         // cairo_move_to(cr, 0, 0); // move to orgin of drawing area
-        // cairo_line_to(cr, 0, WHEEL_SIZE/2 - 10); // draw line to arc of circle.
+        // cairo_line_to(cr, 0, WHEEL_SIZE/2 - 10, i * 2 M_PI / 37, (i + 1) * 2 * M_PI /37); // draw line to arc of circle.
         // cairo_stroke(cr); // color.
 
         // Draw the number for the current slot
@@ -115,14 +100,15 @@ gboolean on_draw(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
         // Restore the context state
         cairo_restore(cr);
     }
-
+    
+    // create hallow  wheel inside roulette wheel.
     cairo_move_to(cr, 0, 0); // Move to center of the wheel
     cairo_set_source_rgb(cr, 0.25, 0.5, 0.25);
     cairo_arc(cr, 0, 0, (WHEEL_SIZE/2 - 10) / 2, 0, 2 * M_PI); // Draw the segment of the wheel
     cairo_close_path(cr); // Close the path to the center
     cairo_fill(cr); // Fill the segment
 
-    // Set the outline color and width
+    // Set the outline color and width.
     cairo_set_line_width(cr, 2); // Width of the outline
     cairo_set_source_rgb(cr, 1, 1, 1);
     cairo_arc(cr, 0, 0, (WHEEL_SIZE/2 - 10) / 2, 0, 2 * M_PI);
@@ -193,79 +179,18 @@ gboolean update_wheel(gpointer user_data) {
         }
 
     }
+
+    else {
+
+        state->angle += state->angle;
+
+    }
     return TRUE;  // Always return TRUE to keep the timer running
 }
+
 /*
-Main function where the main program will run.
-
-This involves declaring nessecary variables, 
-initializing the GTK library and creating the
-window for the user to interact with.
+ create the wheel and return it to the main file.
 */
-// int main(int argc, char *argv[]) {
-    
-//     // try to initiate gtk library, if unsuccessful then exit.
-//     if (!gtk_init_check(&argc, &argv)) {
-//         fprintf(stderr, "Failed to initialize GTK\n");
-//         return 1;
-//     }
-    
-//     printf("GTK initialized\n");
-    
-//     // create window. if unsuccessful then exit.
-//     GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-//     if (!window) {
-//         fprintf(stderr, "Failed to create window\n");
-//         return 1;
-//     }
-    
-//     printf("Window created\n");
-    
-//     // create drawing widget.
-//     GtkWidget *drawing_area = gtk_drawing_area_new();
-//     if (!drawing_area) {
-//         fprintf(stderr, "Failed to create drawing area\n");
-//         return 1;
-//     }
-    
-//     printf("Drawing area created\n");
-    
-//     // create wheel.
-//     WheelState *state = g_new(WheelState, 1);
-//     state->angle = 0.03;
-//     state->spinning = 1;
-//     state-> spin_duration = 0;
-//     state->drawing_area = drawing_area;
-    
-//     // set size of widgets.
-//     gtk_widget_set_size_request(drawing_area, WHEEL_SIZE, WHEEL_SIZE);
-//    // connect signal to interaction.
-//    g_signal_connect(G_OBJECT(drawing_area), "draw", G_CALLBACK(on_draw), state);
-   
-//     // addd drawing widget to window widget.
-//     gtk_container_add(GTK_CONTAINER(window), drawing_area);
-    
-//     // refresh and update wheel rotation.
-//     g_timeout_add(16, update_wheel, state);
-    
-
-//     // connect signal to user interaction to exit.
-//     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    
-//     // visualize all widgets.
-//     gtk_widget_show_all(window);
-    
-//     printf("Starting main loop\n");
-//     gtk_main();
-    
-//     printf("Main loop ended\n");
-    
-//     // free memory.
-//     g_free(state);
-    
-//     return 0;
-// }
-
 GtkWidget* create_roulette_wheel(void) {
 
     // Seed the random number generator once in main
@@ -273,7 +198,8 @@ GtkWidget* create_roulette_wheel(void) {
     
     GtkWidget *drawing_area = gtk_drawing_area_new();
     gtk_widget_set_size_request(drawing_area, WHEEL_SIZE, WHEEL_SIZE);
-
+    
+  // set up the wheel variables.
     WheelState *state = g_new(WheelState, 1);
     state->angle = 0.03;
     state->spinning = 1;
