593645562f7aa3f250c1f9d068a9a30728957f27
Jordan Buckindale <94644161+jordanbuckindale@users.noreply.github.com>
2024-08-23 21:03:48 -0700

    added new function and struct variables to header file.

--- wheel.h
+++ wheel.h
@@ -9,15 +9,23 @@ typedef struct {
     double initial_speed;
     double spin_duration;
     GtkWidget *drawing_area;
+    int spin_state;
 } WheelState;
 
 typedef struct {
     GtkWidget *drawing_area; //-> *drawing_area (GTK widget pointer)
 } betting_table;
 
+
+// WHEEL.C
 GtkWidget* create_roulette_wheel(void);
 gboolean update_wheel(gpointer user_data);
+void trigger_wheel_spin(WheelState *state);
 
+// BETS.C 
 GtkWidget* create_roulette_table(void);
+
+// CHIPS.C
+GtkWidget* create_roulette_chips(void);
 
 #endif // WHEEL_H
